54741f6b81d53c15fd6f5a4520ea8dde65274c45
Sulakshan Sivakumaran <sulakshansivak@gmail.com>
2024-05-09 19:33:33 -0400

    connected server to client

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,30 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from 'react';
+import Navbar from './Navbar';
+import Axios from 'axios';
 
 const Home = () => {
+
+  const getSquadStats = (clubName) => {
+    Axios.post("http://localhost:4000/clubStat/getClubStats", {
+      clubName: clubName
+    }).then(response => {
+      const stats = response.data.data; 
+      console.log(stats);
+    }).catch(error => {
+      console.error(error);
+    });
+  };
+
   return (
     <div className='home'>
-        <Navbar/>
+      <Navbar/>
+      <select name="club" id="club">
+        <option value="" selected disabled hidden>Select Club</option>
+        <option onClick={getSquadStats("Arsenal")} value="Arsenal">Arsenal</option>
+        <option onClick={getSquadStats("Manchester City")} value="Manchester City">Manchester City</option>
+      </select>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
